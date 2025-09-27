666664ad103644f59b31cfc13e8c92ec8d1d6672
Justin Joshi <justinjoshi@proton.me>
2025-09-27 07:26:23 -0400

    Update solution - CodeHub

--- 8-kyu/rock-paper-scissors/rock-paper-scissors.js
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.js
@@ -4,5 +4,7 @@ const rps = (p1, p2) => {
     return "Player 1 won!"
   }else if(p2 === "rock" && p1 === "scissors" || p2 === "paper" && p1 === "rock" || p2 === "scissors" && p1 === "paper"){
     return "Player 2 won!"
+  }else{
+    return "Draw!"
   }
 };
